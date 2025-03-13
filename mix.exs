6410e739ef1c9abf9905bd65e594dd8a88f44977
Evan Purcer <ep@e2.gg>
2025-03-13 12:52:36 -0700

    v0.1.3

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule RegexFormatter.MixProject do
   use Mix.Project
 
   @name "RegexFormatter"
-  @version "0.1.2"
+  @version "0.1.3"
   @repository "https://github.com/evnp/regex_formatter"
 
   defp description() do
